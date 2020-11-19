998de8be69e7133e20676151c9d9a301d3a2cce1
A-Rehman01 <arehmana377@gmail.com>
2020-11-19 21:32:13 +0500

    Connect MongoDB

---
+++ Config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURL");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Database Connected");
+  } catch (err) {
+    console.error(err.message);
+    //Exit Process with failure
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
