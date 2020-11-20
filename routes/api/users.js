e5787a4252aa3ee0a56ce4b064bc18822d3da89a
A-Rehman01 <arehmana377@gmail.com>
2020-11-20 16:41:23 +0500

    Add userPost api and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-//@route GET /api/users
-//@desc Test route
+//@route POST /api/users
+//@desc Registration of User
 //@access Public
 
-router.get("/", (req, res) => res.send("User API"));
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "please include the valid email").isEmail(),
+    check("password", "password must be atleast 6 digit long").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    //   console.log(req.body);
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() });
+    }
+    res.send("User API");
+  }
+);
 
 module.exports = router;
