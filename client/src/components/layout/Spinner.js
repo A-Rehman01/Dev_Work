1a8ef8f78114b880577af0c428240b0d7fe9a539
A-Rehman01 <arehmana377@gmail.com>
2020-12-27 19:50:27 +0500

    Prepare to deployment

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,14 +1,10 @@
 import { Fragment } from "react";
 import spinner from "./spinner-gif.gif";
-
-export default Spinner = () => {
-  return (
-    <Fragment>
-      <img
-        src={spinner}
-        style={{ width: "200px", margin: "auto", display: "block" }}
-        alt=""
-      />
-    </Fragment>
-  );
-};
+export default () => (
+  <Fragment>
+    <img
+      src={spinner}
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
