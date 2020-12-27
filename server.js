28c2b46adef890bef77af006817c19f823316240
A-Rehman01 <arehmana377@gmail.com>
2020-12-27 19:15:12 +0500

    Prepare For Deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./Config/db");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 //Database Connection
 connectDB();
@@ -19,10 +20,10 @@ app.use("/api/profile", require("./routes/api/profile"));
 if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${PORT} PORT`));
